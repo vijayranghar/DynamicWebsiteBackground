94a975a585135f5ba9470255b093993c030d1f4b
vijayranghar <vijayscode@gmail.com>
2016-10-22 15:42:09 +0530

    added background image from local storage

--- index.js
+++ index.js
@@ -11,16 +11,30 @@ $(function(){
     document.getElementById("image-list").addEventListener('click',function(e){
         if(e.target.nodeName== "SPAN")
         {
-            var background = e.target.previousSibling;
+            var background_url = e.target.previousSibling; // get background url
             var element = document.getElementsByClassName('wrapper');
-            console.log(element)
-            window.localStorage('background-image':element);
-            for(var i= 0; i<element.length;i++)
-            console.log(element[i].style.backgroundImage="url('"+background + "')");
+            console.log(element+">>>")
+            localStorage.setItem('background-image',background_url);
+            for(var i= 0; i<element.length;i++){
+                console.log(element[i].style.backgroundImage="url('"+background_url + "')");
+            }
+
         }
     })
+    var wrapper = document.getElementsByClassName('wrapper');
+
+    /* set default background in local storage */
+    if((localStorage.getItem("background-image")) == null){
+        for(var i=0; i<wrapper.length; i++)
+            wrapper[i].style.backgroundImage= "url("+localStorage.getItem('background-image')+ ")";
+    }
+    else{
+        for(var i=0; i<wrapper.length; i++)
+            wrapper[i].style.backgroundImage= "url("+localStorage.getItem('background-image')+ ")";
+    }
 });
 
+/* AJAX fetch images request */
 function fetch_images(){
     var searchValue = $(".form-searchImage input").val();
     var API_KEY = "3374682-d1aa875b8db23aaadcf236ea3";
@@ -34,18 +48,18 @@ function fetch_images(){
         for(var i=0;i<data.hits.length;i++) {
             //console.log(data);
             thumbnail_image = data.hits[i].webformatURL; // get thumnail url
-            resized_thumbnail_image = thumbnail_image.replace(/_640/g,'_340');
+            resized_thumbnail_image = thumbnail_image.replace(/_640/g, '_340');
             image_full = data.hits[i].webformatURL;
             var element = "<li> " +
                 "<a target='_blank' style='background-image: url(" + resized_thumbnail_image + ")' href=" + image_full + "></a>" +
-                "<span class='set-background'>Set as background</span> </li>" ;
+                "<span class='set-background'>Set as background</span> </li>";
             $("#image-list").append(element);
         }
-        callback(data);
     })
 
 }
 
+/* show popup after search */
 function showPopup(){
     document.getElementById("popup").style.display="block";
     //popup close
@@ -54,11 +68,4 @@ function showPopup(){
         popup.style.display="none";
     })
 }
-var callback = function (data) {
-    console.log(data);
-}
 
-window.localStorage(showPopup()-image:"https://drscdn.500px.org/photo/87206859/m%3D2048/c9128d2ca772fa229f6d30405b57a61e");
-var wrapper = document.getElementsByClassName('wrapper')
-for(i=0;i<wrapper.length;i++)
-wrapper[i].style.backgroundImage=localStorage.getItem('background-image')
